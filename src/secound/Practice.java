ec940d34d215c969de32d813d2a4a29e1392bf07
Abdullah Rafi <abdllahrfi@gmail.com>
2022-05-19 15:09:27 -0400

    first commit

---
+++ src/secound/Practice.java
@@ -0,0 +1,31 @@
+package secound;
+
+public class Practice {
+	// example of variable initiated.
+	public String name = "Abdullah Ar Rafi";
+	public byte age = 30;
+	public short rent = 2400;
+	public int areaCode = 1917;
+	public long accountNumber = 123456l;
+	public float weight = 1.2345f;
+	public double grade = 3.4556668;
+	public char sex = 'M';
+	public boolean married = true;
+	public static void main(String[] args) {
+		
+	Practice practice = new Practice();
+	System.out.println("My Name is: " + practice.name + "\nMy age is:" + practice.age + "\nMy House Rent Is:" + practice.rent);
+		
+		
+	}
+	
+		
+	
+	
+	
+		
+	}
+	
+	
+
+
